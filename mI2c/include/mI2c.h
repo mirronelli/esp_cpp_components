b7a859131ceadf94f8ea43e0b1f960a6c1dc7bb9
Mirronelli <miroslav.moravek@outlook.com>
2019-07-29 13:01:03 +0200

    added 16bit register read/write methods

--- mI2c/include/mI2c.h
+++ mI2c/include/mI2c.h
@@ -9,8 +9,14 @@ class mI2cMaster
 public:
 	mI2cMaster(i2c_port_t port, gpio_num_t pinSda, gpio_num_t pinClk, uint8_t deviceAddress, uint32_t frequency);
 	~mI2cMaster();
+
 	uint8_t ReadRegister(uint8_t registerAddress);
+	uint16_t ReadRegister16(uint8_t registerAddress);
+
 	void WriteRegister(uint8_t registerAddress, uint8_t data);
+	void WriteRegister16(uint8_t registerAddress, uint16_t data);
+
+	bool Detect();
 
 private:
 	i2c_port_t port;
--- mI2c/mI2c.cpp
+++ mI2c/mI2c.cpp
@@ -1,5 +1,6 @@
 #include "include/mI2c.h"
 #include "exception"
+#include "esp_log.h"
 
 mI2cMaster::mI2cMaster(i2c_port_t port, gpio_num_t pinSda, gpio_num_t pinClk, uint8_t deviceAddress, uint32_t frequency)
 	: port{port}, pinSda{pinSda}, pinClk{pinClk}, deviceAddress{deviceAddress}, frequency{frequency}
@@ -46,10 +47,47 @@ uint8_t mI2cMaster::ReadRegister(uint8_t registerAddress)
 	result = i2c_master_cmd_begin(port, cmd, 50 / portTICK_RATE_MS);
 	i2c_cmd_link_delete(cmd);
 
-	if (result != ESP_OK)
-	throw std::exception();
+	// if (result != ESP_OK)
+	// 	throw std::exception();
 
 	return data;
+}
+
+uint16_t mI2cMaster::ReadRegister16(uint8_t registerAddress)
+{
+	uint8_t data1 = 0;
+	uint8_t data2 = 0;
+	uint16_t resultData = 0;
+
+	i2c_cmd_handle_t cmd;
+	esp_err_t result;
+
+	cmd = i2c_cmd_link_create();
+
+	// Write register address to device
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (deviceAddress << 1) | I2C_MASTER_WRITE, ACK_REQUIRED);
+	i2c_master_write_byte(cmd, registerAddress, ACK_REQUIRED);
+
+	// Read from register
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (deviceAddress << 1) | I2C_MASTER_READ, ACK_REQUIRED);
+	i2c_master_read_byte(cmd, &data1, I2C_MASTER_ACK);
+	i2c_master_read_byte(cmd, &data2, I2C_MASTER_ACK);
+
+	// Write stop sequence
+	i2c_master_stop(cmd);
+
+	// Execute queue and destroy cmd
+	result = i2c_master_cmd_begin(port, cmd, 50 / portTICK_RATE_MS);
+	i2c_cmd_link_delete(cmd);
+
+	resultData = data1 << 8 | data2;
+
+	// if (result != ESP_OK)
+	// 	throw std::exception();
+
+	return resultData;
 }
 
 void mI2cMaster::WriteRegister(uint8_t registerAddress, uint8_t data)
@@ -74,4 +112,65 @@ void mI2cMaster::WriteRegister(uint8_t registerAddress, uint8_t data)
 
 	if (result != ESP_OK)
 		throw std::exception();
+}
+
+void mI2cMaster::WriteRegister16(uint8_t registerAddress, uint16_t data)
+{
+	i2c_cmd_handle_t cmd;
+	esp_err_t result;
+	uint8_t msb = data >> 8;
+	uint8_t lsb = data & 0xff;
+
+	ESP_LOGI("mI2c", "MSB %x", msb);
+	ESP_LOGI("mI2c", "LSB: %x", lsb);
+
+	cmd = i2c_cmd_link_create();
+
+	// Write register address to device, followed by msb an lsb
+	i2c_master_start(cmd);
+
+	i2c_master_write_byte(cmd, (deviceAddress << 1) | I2C_MASTER_WRITE, ACK_REQUIRED);
+	i2c_master_write_byte(cmd, registerAddress, ACK_REQUIRED);
+	i2c_master_write_byte(cmd, msb, ACK_REQUIRED);
+	i2c_master_write_byte(cmd, lsb, ACK_REQUIRED);
+
+	// Write stop sequence
+	i2c_master_stop(cmd);
+
+	// Execute queue and destroy cmd
+	result = i2c_master_cmd_begin(port, cmd, 50 / portTICK_RATE_MS);
+	i2c_cmd_link_delete(cmd);
+
+	if (result != ESP_OK){
+		ESP_LOGI("mI2c", "error writing register: %d", result);
+		//throw std::exception();
+	}
+}
+
+bool mI2cMaster::Detect()
+{
+	i2c_cmd_handle_t cmd;
+	esp_err_t result;
+
+	cmd = i2c_cmd_link_create();
+
+	// Write register address to device anb wait for ack
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (deviceAddress << 1) | I2C_MASTER_WRITE, ACK_REQUIRED);
+	i2c_master_stop(cmd);
+
+	// Execute queue and destroy cmd
+	result = i2c_master_cmd_begin(port, cmd, 50 / portTICK_RATE_MS);
+	i2c_cmd_link_delete(cmd);
+
+	if (result != ESP_OK)
+	{
+		ESP_LOGI("mI2c", "Device NOT detected: %d", deviceAddress);
+		return false;
+	}
+	else
+	{
+		ESP_LOGI("mI2c", "Device detected: %d", deviceAddress);
+		return true;
+	}
 }
